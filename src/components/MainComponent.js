10d9a0ea505534668c446abdd19eb07ce1920bcd
iseungho <ghehf51@naver.com>
2025-03-14 17:47:00 +0900

    init

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function MainComponent(props) {
+
+    return (
+        <div>
+            <h1>React App</h1>
+            <p>React App with Webpack 5</p>
+        </div>
+    );
+}
+
+export default MainComponent;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainComponent from '../components/MainComponent';
+
+const MainPage = () => {
+    return (
+        <>
+            <MainComponent/>
+        </>
+    );
+}
+
+export default MainPage;
