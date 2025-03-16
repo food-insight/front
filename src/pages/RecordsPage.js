af5de6a71ce1f8c4ccf721638bee2f02ed93e86b
iseungho <ghehf51@naver.com>
2025-03-16 23:31:45 +0900

    feat: add to RecordsPage
    식단 기록 페이지입니다.

---
+++ src/pages/RecordsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecordsComponent from '../components/RecordsComponent';
+import BasicLayout from '../layouts/BasicLayout';
+
+const MainPage = () => {
+    return (
+        <BasicLayout>
+            <RecordsComponent />
+        </BasicLayout>
+    );
+}
+
+export default MainPage;
--- src/router/root.js
+++ src/router/root.js
@@ -5,12 +5,17 @@ const { createBrowserRouter } = require("react-router-dom");
 const Loading = <div className="loading-image"></div>
 
 const Main = lazy(() => import("../pages/MainPage"))
+const Records = lazy(() => import("../pages/RecordsPage"))
 
 const root = createBrowserRouter([
     {
         path: "",
         element: <Suspense fallback={Loading}><Main/></Suspense>
     },
+    {
+        path: "/records",
+        element: <Suspense fallback={Loading}><Records/></Suspense>
+    }
 ])
 
 export default root
