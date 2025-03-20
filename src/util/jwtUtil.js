c30480f715e786f002e466aa3698f127554071c9
iseungho <ghehf51@naver.com>
2025-03-20 22:36:07 +0900

    feat: JWT 토큰 적용

--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -1,88 +1,53 @@
 import axios from "axios";
-import { getCookie, setCookie } from "./cookieUtil";
-import host from "../api/apiConfig";
+import { getCookie } from "./cookieUtil";
 
-const jwtAxios = axios.create()
-
-const refreshJWT = async (accessToken, refreshToken) => {
-
-    const prefix = host
-
-    const header = { headers: { "Authorization": `Bearer ${accessToken}` } }
-
-    const res = await axios.get(`${prefix}/member/refresh?refreshToken=${refreshToken}`, header)
-
-    return res.data
-}
+const jwtAxios = axios.create();
 
 // before request
 const beforeReq = (config) => {
+    const accessToken = getCookie("accessToken");
+    const refreshToken = getCookie("refreshToken");
 
-    const memberInfo = getCookie("member")
-
-    if (!memberInfo) {
-        console.error("Member NOT FOUND")
-        return Promise.reject(
-            {
-                response:
-                {
-                    data:
-                    {
-                        error: "REQUIRE_LOGIN"
-                    }
+    if (!accessToken || !refreshToken) {
+        console.error("Tokens NOT FOUND");
+        return Promise.reject({
+            response: {
+                data: {
+                    error: "REQUIRE_LOGIN"
                 }
             }
-        )
+        });
     }
 
-    const { accessToken } = memberInfo
-
-    //  Authorization 헤더 처리
-    config.headers.Authorization = `Bearer ${accessToken}`
-    return config
-}
+    // Authorization header 처리
+    config.headers.Authorization = `${accessToken}`;
+    return config;
+};
 
 // fail request
 const requestFail = (err) => {
-    console.error("request error............")
-    return Promise.reject(err)
-}
+    console.error("request error............");
+    return Promise.reject(err);
+};
 
 // before return response
 const beforeRes = async (res) => {
-    const data = res.data
+    const data = res.data;
 
     if (data && data.error === 'ERROR_ACCESS_TOKEN') {
-
-        const memberCookieValue = getCookie("member")
-
-        const result = await refreshJWT(memberCookieValue.accessToken, memberCookieValue.refreshToken)
-
-        memberCookieValue.accessToken = result.accessToken
-        memberCookieValue.refreshToken = result.refreshToken
-
-        setCookie("member", JSON.stringify(memberCookieValue))
-
-        const originalRequest = res.config
-
-        originalRequest.headers.Authorization = `Bearer ${result.accessToken}`
-
-        return axios(originalRequest);
-
+        // Handle token error, possibly refresh token logic here
     }
 
-    return res
-}
-
+    return res;
+};
 
 // fail response
 const responseFail = (err) => {
-    console.error("response fail error.............")
+    console.error("response fail error.............");
     return Promise.reject(err);
-}
+};
 
-jwtAxios.interceptors.request.use(beforeReq, requestFail)
+jwtAxios.interceptors.request.use(beforeReq, requestFail);
+jwtAxios.interceptors.response.use(beforeRes, responseFail);
 
-jwtAxios.interceptors.response.use(beforeRes, responseFail)
-
-export default jwtAxios
+export default jwtAxios;
